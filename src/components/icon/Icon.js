fd934839477ae41fd8b0037702f1053238240a56
Nischal Gautam <nischal1024@gmail.com>
2018-03-01 16:34:02 +1100

    icon

---
+++ src/components/icon/Icon.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+class Icon extends Component {
+  render() {
+    return (
+      <View>
+        <Text > Icon component </Text>
+      </View>
+    );
+  }
+}
+
+export default Icon;
