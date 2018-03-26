f3139afdf2fc0c067e678b6a588491198379b6e5
Nischal Gautam <nischal1024@gmail.com>
2018-03-26 17:17:59 +1100

    integrated theme with icons on tab icons

--- src/config/Route.js
+++ src/config/Route.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StackNavigator, TabNavigator, TabBarBottom } from 'react-navigation';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import theme from '../theme';
 import {
   ProfileScreen,
   ProfileSetupScreen,
@@ -25,8 +26,23 @@ const LoggedInScreen = TabNavigator(
   },
 
   {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Settings') {
+          iconName = `ios-options${focused ? '' : '-outline'}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      },
+    }),
     tabBarOptions: {
-      activeTintColor: 'tomato',
+      activeTintColor: theme.colors.primary,
       inactiveTintColor: 'gray',
     },
     tabBarComponent: TabBarBottom,
